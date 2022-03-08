dcd5dccd47c0a70d9da8e3aa948a47716124d009
oguzhanfiliz <oguzhanfiliz3@gmail.com>
2022-03-08 15:32:51 +0300

    start.sh update command

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ apt-get install -y docker.io git
 apt-get install docker-compose -y
 git clone https://github.com/oguzhanfiliz/PathCaseCRUDAPI.git
 cd ./PathCaseCRUDAPI
-docker compose up -d
+docker-compose up -d
