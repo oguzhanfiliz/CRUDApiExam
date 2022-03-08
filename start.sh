20edec1ffed5442c91089c6f0bb9fed16cac847e
oguzhanfiliz <oguzhanfiliz3@gmail.com>
2022-03-08 16:04:19 +0300

    sh update seeder

--- start.sh
+++ start.sh
@@ -1,6 +1,16 @@
 apt-get update 
 apt-get install -y docker.io git
-apt-get install docker-compose -y
+apt-install docker-compose -y
 git clone https://github.com/oguzhanfiliz/PathCaseCRUDAPI.git
 cd ./PathCaseCRUDAPI
+pwd
+
+
+
 docker-compose up -d
+
+
+docker exec -it laravel /bin/sh
+
+
+php artisan migrate --seed
